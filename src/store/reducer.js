f54eb81f56fdfebbd1ec2adf173f266781acdb9a
Abby Low <abby.low@plus65.com.sg>
2020-09-24 00:11:23 +0800

    pass and retrieve data with action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,30 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    if(action.type === 'INCREMENT') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if(action.type === 'DECREMENT') {
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+    }
+    if(action.type === 'ADDITION') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+    if(action.type === 'SUBTRACTION') {
+        return {
+            ...state,
+            counter: state.counter - action.value
+        }
+    }
     return state;
 }
 
