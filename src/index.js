1872bdafe8aa4ad1e13ad0f652faeb83000b1242
Abby Low <abby.low@plus65.com.sg>
2020-09-24 18:24:32 +0800

    add middleware and dev-tool

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux'; // to connect between react and redux
 
 import './index.css';
@@ -16,7 +16,26 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    /* preloadedState, */
+    composeEnhancers(
+        applyMiddleware(logger)
+    ));
+// const store = createStore(rootReducer, applyMiddleware(logger));
 
 // const store = createStore(reducer);
 
